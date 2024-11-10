832ca16dfe50534eaae1cf05147174b6681ede1c
subjerome <subjerome@hotmail.com>
2024-11-10 11:16:09 -0600

    Switch to using MPRLS library

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
+#include <SPI.h>
 #include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <Adafruit_MPRLS.h>
 
 #define LED_PIN    38
-
 // How many NeoPixels are attached to the Arduino?
 #define LED_COUNT 1
-
   // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 // Argument 1 = Number of pixels in NeoPixel strip
@@ -17,6 +18,11 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 int pixelCycle = 0;           // Pattern Pixel Cycle between colors
+
+// You dont *need* a reset and EOC pin for most uses, so we set to -1 and don't connect
+#define RESET_PIN  -1  // set to any GPIO pin # to hard-reset on begin()
+#define EOC_PIN    -1  // set to any GPIO pin to read end-of-conversion by pin
+Adafruit_MPRLS mpr = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
@@ -46,6 +52,10 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Start");
+  Serial.print("SDA = ");
+  Serial.println(SDA + 30);
+  Serial.print("SCL = ");
+  Serial.println(SCL + 30);
 
   rainbow();
  
@@ -53,6 +63,16 @@ void setup() {
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(25); // Set BRIGHTNESS to about 1/5 (max = 255)
+
+  Serial.println("MPRLS Simple Test");
+  if (! mpr.begin()) {
+    Serial.println("Failed to communicate with MPRLS sensor, check wiring?");
+    while (1) {
+      delay(100);
+    }
+  }
+  Serial.println("Found MPRLS sensor");
+
 }
 
 void loop() {
@@ -60,5 +80,13 @@ void loop() {
   rainbow();
   delay(10);
   if (pixelCycle == 0)
+  {
     Serial.println("Red again");
+    Serial.print("Pressure = ");
+    float pressure_hPa = mpr.readPressure();
+    Serial.print("Pressure (hPa): "); 
+    Serial.println(pressure_hPa);
+    Serial.print("Pressure (PSI): "); 
+    Serial.println(pressure_hPa / 68.947572932);
+  }
 }
